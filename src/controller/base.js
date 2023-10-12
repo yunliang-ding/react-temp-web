153528413e2359cee391f958229033acc63e8bef
lighthouse-8G <lighthouse@8g.com>
2023-10-12 10:50:31 +0800

    opt

--- src/controller/base.js
+++ src/controller/base.js
@@ -1,19 +1,20 @@
 const axios = require('axios');
 
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: 'http://server.yunliang.cloud',
+})
+
 module.exports = class extends think.Controller {
   __before() {}
-  CenterServices(){
-    const instance = axios.create({
-      withCredentials: true,
-      baseURL: 'http://server.yunliang.cloud',
+  CenterServices(config){
+    return instance({
+      ...config,
+      headers: {
+        ...config.headers,
+        appid: this.ctx.request.header.appid,
+        cookie: this.ctx.request.header.cookie
+      }
     })
-    instance.interceptors.request.use((requestConfig) => {
-      requestConfig.headers = {
-        Appid: this.ctx.request.header.appid,
-        Cookie: this.ctx.request.header.cookie
-      };
-      return requestConfig;
-    });
-    return instance
   }
 };
--- src/controller/user.js
+++ src/controller/user.js
@@ -1,12 +1,25 @@
-const Base = require('./base.js');
+const Base = require("./base.js");
 
 module.exports = class extends Base {
-  async infoAction(){
-    const { data } = await this.CenterServices().post('/user/userinfo');
-    this.json(data)
+  async infoAction() {
+    const { data } = await this.CenterServices({
+      method: "post",
+      url: "/user/userinfo",
+    });
+    this.json(data);
   }
-  async logoutAction(){
-    const { data } = await this.CenterServices().post('/unification/logout')
-    this.json(data)
+  async logoutAction() {
+    const { appid } = this.ctx.request.header;
+    const { data } = await this.CenterServices({
+      method: "post",
+      url: "/unification/logout",
+    });
+    if (data.code === 200) {
+      // 注销客户端cookie
+      this.cookie(`app_auth_${appid}`, null, {
+        domain: ".yunliang.cloud",
+      });
+    }
+    this.json(data);
   }
 };

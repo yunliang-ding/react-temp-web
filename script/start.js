c03437ee78677b65bade8dce46a0e6065907f38e
lighthouse-8G <lighthouse@8g.com>
2023-10-13 11:43:28 +0800

    fixed

--- script/start.js
+++ script/start.js
@@ -1,7 +1,7 @@
 // 启动前端的服务
 const { port } = require('../www/build.json')
 
-require("child_process").execSync(`cd ./www; icejs start --port ${port} --disable-open --mode local`, {
+require("child_process").execSync(`cd ./www; npm start`, {
   // 遇到服务起不来，可开启下面属性，查看控制台打印信息
-  // stdio: "inherit",
+  stdio: "inherit",
 });

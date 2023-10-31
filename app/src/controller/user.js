04c80bc3916d8148ec9fb6555ddb0928f941cc68
xulei <xulei@lei.com>
2023-10-31 23:08:41 +0800

    fixed

--- app/src/controller/user.js
+++ app/src/controller/user.js
@@ -2,11 +2,15 @@ const Base = require("./base.js");
 
 module.exports = class extends Base {
   async infoAction() {
-    const { data } = await this.CenterServices({
-      method: "post",
-      url: "/user/userinfo",
-    });
-    this.json(data);
+    try {
+      const { data } = await this.CenterServices({
+        method: "post",
+        url: "/user/userinfo",
+      });
+      this.json(data);
+    } catch (error) {
+      console.log(error)
+    }
   }
   async logoutAction() {
     const { appid } = this.ctx.request.header;

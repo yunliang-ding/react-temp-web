f9365254482bbbe0ec3b59cf9221e0bb07bf02e5
xulei <xulei@lei.com>
2023-11-29 13:40:43 +0800

    fixed

--- src/layouts/footer-render.tsx
+++ src/layouts/footer-render.tsx
@@ -4,26 +4,19 @@ import { Icon } from '@/util';
 
 export default () => {
   return (
-    <div
-      style={{
-        height: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        padding: '0 12px',
-      }}
-    >
+    <>
       <Icon
         type="icon-shengyin"
         style={{ color: 'rgb(var(--primary-6))', fontSize: 20 }}
       />
       <div style={{ width: 'calc(100% - 100px)', fontWeight: 'bold' }}>
         <Marquee pauseOnHover gradient={false} delay={2}>
-          xxx消息通知
+          介绍：xxx消息通知
         </Marquee>
       </div>
       <Button size="small" type="text">
         版本号 1.0.0
       </Button>
-    </div>
+    </>
   );
 };

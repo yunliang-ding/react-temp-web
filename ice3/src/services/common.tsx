c84d7a66c99180b42b3e32415d6cc55a2898b241
xulei <xulei@lei.com>
2023-12-01 15:14:33 +0800

    fixed

--- ice3/src/services/common.tsx
+++
@@ -1,9 +0,0 @@
-import { request } from 'ice';
-
-export const outLogin = () => {
-  return request.post('/user/logout');
-};
-
-export const userInfo = () => {
-  return request.post('/user/info');
-};
--- ice3/src/store/breadcrumb.ts
+++
@@ -1,8 +0,0 @@
-import { CreateStore } from 'react-core-form-store';
-
-export default CreateStore({
-  title: '', // 大标题
-  breadcrumb: undefined, // 面包屑
-  extra: null,
-});
-

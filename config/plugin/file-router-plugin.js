a280d1e545031aafffecee569d8c362e35cb0539
xulei <xulei@lei.com>
2023-12-05 09:55:10 +0800

    fixed

--- config/plugin/file-router-plugin.js
+++ config/plugin/file-router-plugin.js
@@ -12,7 +12,9 @@ let initialFlag = false;
 /** 创建文件路由 */
 const folder = path.resolve(__dirname, '../../src/pages/**/*.tsx');
 const output = path.resolve(__dirname, '../../src/.app/router.tsx');
-const createFileRouter = (ignorePaths = ['component/', 'components/']) => {
+const createFileRouter = async (
+  ignorePaths = ['component/', 'components/'],
+) => {
   const files = glob.sync(folder);
   const importArr = [];
   const routes = files
@@ -20,18 +22,20 @@ const createFileRouter = (ignorePaths = ['component/', 'components/']) => {
       return !ignorePaths.some((i) => file.includes(i));
     })
     .map((file) => {
-      const routeName = path.basename(file, path.extname(file));
       let filePath = file.split('/src/pages')[1];
       filePath = filePath.substring(0, filePath.lastIndexOf('.'));
       if (filePath.endsWith('/index')) {
         filePath = filePath.substring(0, filePath.length - 6); // 移除 index
       }
-      const CompName = `${filePath.replaceAll('/', '').replaceAll('$', '')}`.split('');
+      const CompName = `${filePath
+        .replaceAll('/', '')
+        .replaceAll('$', '')
+        .replaceAll(' ', '')}`.split('');
       // 字母开头
-      if(/[a-zA-Z]/.test(CompName[0])){
+      if (/[a-zA-Z]/.test(CompName[0])) {
         CompName[0] = CompName[0].toUpperCase();
       } else {
-        CompName.unshift('R')
+        CompName.unshift('R');
       }
       importArr.push(`import ${CompName.join('')} from '@/pages${filePath}';`); // 添加依赖
       return {
@@ -40,11 +44,11 @@ const createFileRouter = (ignorePaths = ['component/', 'components/']) => {
       };
     });
   routerConfig = `export default ${decodeStr(JSON.stringify(routes, null, 2))}`;
+  const content = `${importArr.join('\n')}\n\n${routerConfig}`;
   const outputFilePath = path.resolve(output);
-  fs.outputFile(
-    outputFilePath,
-    `${importArr.join('\n')}\n\n${routerConfig}`,
-  );
+  fs.removeSync(outputFilePath);
+  await new Promise((res) => setTimeout(res, 100));
+  fs.outputFile(outputFilePath, content);
 };
 
 class FileRouterPlugin {
@@ -55,12 +59,16 @@ class FileRouterPlugin {
     compiler.hooks.environment.tap('FileRouterPlugin', () => {
       if (initialFlag === false) {
         createFileRouter(this.options.ignorePaths); // 首次编译先创建一次
-        const watcher = chokidar.watch('src', {
+        const watcher = chokidar.watch('src/pages', {
           ignored: /node_modules/,
           ignoreInitial: true,
         });
-        watcher.on("add", (path) => {
-          console.log('\x1B[32m%s\x1B[0m', `新增文件: ${path}`)
+        watcher.on('add', async (path) => {
+          console.log('\x1B[32m%s\x1B[0m', `新增文件: ${path}`);
+          createFileRouter(this.options.ignorePaths);
+        });
+        watcher.on('unlink', async (path) => {
+          console.log('\x1B[31m%s\x1B[0m', `删除文件: ${path}`);
           createFileRouter(this.options.ignorePaths);
         });
       }

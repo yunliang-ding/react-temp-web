d61f52d79e59873802a681575eb1c728a75a1b6f
xulei <xulei@lei.com>
2023-12-28 17:52:39 +0800

    fix

--- development.ts
+++ development.ts
@@ -1,12 +1,10 @@
-const path = require('path');
+//@ts-ignore
 const Application = require('thinkjs');
 const watcher = require('think-watcher');
 
-const instance = new Application({
+new Application({
   ROOT_PATH: __dirname,
-  APP_PATH: path.join(__dirname, 'src'),
+  APP_PATH: `${__dirname}/src`,
   watcher: watcher,
   env: 'development'
-});
-
-instance.run();
+}).run();
---
+++ production.ts
@@ -0,0 +1,9 @@
+//@ts-ignore
+const Application = require('thinkjs');
+
+new Application({
+  ROOT_PATH: __dirname,
+  APP_PATH: `${__dirname}/src`,
+  proxy: true, // use proxy
+  env: 'production'
+}).run();

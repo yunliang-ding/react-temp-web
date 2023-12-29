f472196184ec0002a99e056544dd22c76d827b0f
xulei <xulei@lei.com>
2023-12-29 13:19:40 +0800

    fixed

--- development.js
+++ development.js
@@ -1,9 +1,21 @@
 const Application = require('thinkjs');
 const watcher = require('think-watcher');
 
-new Application({
+/** 创建实例 */
+const instance = new Application({
   ROOT_PATH: __dirname,
-  APP_PATH: `${__dirname}/src`,
-  watcher: watcher,
-  env: 'development'
-}).run();
+  APP_PATH: `${__dirname}/src/apis`,
+  env: 'development',
+});
+
+/** 监听 apis */
+new watcher(
+  {
+    srcPath: `${__dirname}/src/apis`,
+  },
+  (fileInfo) => {
+    instance._watcherCallBack(fileInfo);
+  },
+).watch();
+
+instance.run(); // run server
--- production.js
+++ production.js
@@ -2,7 +2,7 @@ const Application = require('thinkjs');
 
 new Application({
   ROOT_PATH: __dirname,
-  APP_PATH: `${__dirname}/src`,
+  APP_PATH: `${__dirname}/src/apis`,
   proxy: true, // use proxy
   env: 'production'
 }).run();

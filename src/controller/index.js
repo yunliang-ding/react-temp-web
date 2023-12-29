dc8ead875ee3fdb6b31fc1bedf870bc441a92e04
xulei <xulei@lei.com>
2023-12-29 11:38:28 +0800

    fixed

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,14 +1,9 @@
-const { readFileSync } = require('fs');
-const path = require('path');
+const { readFileSync: rfs } = require('fs');
 
 module.exports = class extends think.Controller {
   indexAction() {
-    const content = readFileSync(
-      path.resolve(
-        __dirname,
-        think.env === 'production' ? '../../www/build/index.html' : '../../www/dev/index.html',
-      ),
-    ).toString();
-    this.ctx.res.end(content);
+    const mode = think.env === 'production' ? 'build' : 'dev';
+    const body = rfs(`${think.ROOT_PATH}/www/${mode}/index.html`).toString();
+    this.ctx.res.end(body);
   }
 };

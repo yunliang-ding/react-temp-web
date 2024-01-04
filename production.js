a1f4b27fea69b2ff16ee92f6d463b21b396e0e87
xulei <xulei@lei.com>
2024-01-04 14:45:06 +0800

    fixed

--- production.js
+++
@@ -1,8 +0,0 @@
-const Application = require('thinkjs');
-
-new Application({
-  ROOT_PATH: __dirname,
-  APP_PATH: `${__dirname}/src/apis`,
-  proxy: true, // use proxy
-  env: 'production'
-}).run();
